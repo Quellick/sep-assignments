b0fafea4fafb93bf155cab7594f39a3e9c92d991
Tucker Platt <Tucker@bendcable.com>
2018-06-26 15:56:31 -0700

    complete intro-answer.txt

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -6,29 +6,41 @@ class Line
 
   def initialize
     self.members = []
+
   end
 
   def join(person)
+    self.members << person
   end
 
   def leave(person)
+    self.members.delete(person)
+    ##move up people in the line?
   end
 
   def front
+    self.members.first
   end
 
   def middle
+    midpoint = (self.members.length/2).ceil
+    self.members[midpoint]
   end
 
   def back
+    self.members.last
   end
 
   def search(person)
+    if index(person)
+      person
+    end
   end
 
   private
 
   def index(person)
+    self.members.find_index(person)
   end
 
 end
