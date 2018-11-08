655ac04804da47f4f0b6579861f30ab551eb45a8
Quellick <Quellick@gmail.com>
2018-11-08 11:36:46 -0800

    searching complete

---
+++ 02-algorithms-and-complexity/02-algorithms-searching/fibonacci_recursive.rb
@@ -0,0 +1,33 @@
+def fib_r(n)
+	if n == 0
+		return 0
+	elsif n == 1
+		return 1
+	else
+		return fib_r(n-1) + fib_r(n-2)
+	end
+end
+
+def fib_i(n)
+	return 0 if n == 0
+	return 1 if n == 1
+	fib0 = 0
+	fib1 = 1
+	for n in 1..n-1 do
+		temp = fib0
+		fib0 = fib1
+		fib1 = temp + fib1
+	end
+	return fib1
+end
+
+puts "fib_r(0): #{fib_r(0)}"
+puts "fib_r(1): #{fib_r(1)}"
+puts "fib_r(2): #{fib_r(2)}"
+puts "fib_r(3): #{fib_r(3)}"
+puts "fib_r(4): #{fib_r(4)}"
+puts "fib_r(5): #{fib_r(5)}"
+puts "fib_r(6): #{fib_r(6)}"
+puts "fib_r(7): #{fib_r(7)}"
+puts "fib_r(8): #{fib_r(8)}"
+puts "fib_r(9): #{fib_r(9)}"
